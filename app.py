d738d959f09cec7bf91ca2ee545adc9aba603981
Laurente64 <lucas.laurente@germinare.org.br>
2025-09-23 10:55:46 -0300

    Adicionando versionamento automático

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Olá, Mundo! Imagem Docker publicada com sucesso via GitHub Actions!</h1>'
+
+if __name__ == '__main__':
+    
+    app.run(host='0.0.0.0', port=5000)
+
+# pipeline -> on -> action -> branches[str] | jobs -> job -> runs-on | steps -> step -> name/uses/(with)
